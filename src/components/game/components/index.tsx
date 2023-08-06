3478fc57693835341377ac22e3a2ec92949569a3
NikoletaNedeljkovic <nikoleta.nedeljkovic.pa@gmail.com>
2023-08-06 18:07:23 +0200

    Fixed untracked files

--- src/components/game/components/index.tsx
+++
@@ -1,6 +0,0 @@
-export { default as CheckAndDeleteButtons } from './CheckAndDeleteButtons'
-export { default as CorrectNumberPosition } from './CorrectNumberPosition'
-export { default as GameBoard } from './GameBoard'
-export { default as GameScore } from '../../gameScore/GameScore'
-export { default as NumberButtons } from './NumberButtons'
-export { default as Result } from './Result'
